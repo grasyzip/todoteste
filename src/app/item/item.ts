89d19769a54982838859cfabbe18fb4ee773a7ff
Grasielly R <159390727+grasyzip@users.noreply.github.com>
2026-04-23 16:53:16 -0300

    Update item.ts

--- src/app/item/item.ts
+++ src/app/item/item.ts
@@ -1,57 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { Tarefa } from './tarefa';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Tarefa } from "../tarefa";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-item',
+  standalone: false,
+  templateUrl: './item.html',
+  styleUrl: './item.css',
 })
-export class AppComponent implements OnInit {
-  tarefas: Tarefa[] = [];
-  
-  async ngOnInit() {
-    await this.carregarTarefas();
+export class Item {
+  emEdicao = false;
+  mostrarmodal = false;
+  @Input() tarefa: Tarefa = new Tarefa ("", false);
+  @Output() remover = new EventEmitter<Tarefa>();
+  @Output() modificaTarefa = new EventEmitter();
+
+  abrirModalExclusao() {
+    this.mostrarmodal = true;
   }
-  
-  // Carregar tarefas sem cache
-  async carregarTarefas() {
-    try {
-      const url = 'https://passionate-simplicity-production-0313.up.railway.app/api/getAll';
-      const response = await fetch(`${url}?_=${Date.now()}`, {
-        method: 'GET',
-        headers: {
-          'Cache-Control': 'no-cache, no-store, must-revalidate',
-          'Pragma': 'no-cache'
-        }
-      });
-      
-      if (response.ok) {
-        this.tarefas = await response.json();
-        console.log('Tarefas carregadas:', this.tarefas.length);
-      }
-    } catch (error) {
-      console.error('Erro ao carregar:', error);
-    }
+
+  cancelarExclusao() {
+    this.mostrarmodal = false;
   }
-  
-  // Método chamado quando confirma exclusão no modal
-  async removerTarefa(tarefa: Tarefa) {
-    try {
-      const response = await fetch(`https://passionate-simplicity-production-0313.up.railway.app/api/delete/${tarefa.id}`, {
-        method: 'DELETE',
-        headers: {
-          'Cache-Control': 'no-cache'
-        }
-      });
-      
-      if (response.ok) {
-        // Recarregar a lista após excluir
-        await this.carregarTarefas();
-        console.log('Tarefa excluída com sucesso');
-      } else {
-        console.error('Erro ao excluir:', await response.text());
-      }
-    } catch (error) {
-      console.error('Erro na requisição:', error);
-    }
+
+  confirmarExclusao() {
+    this.mostrarmodal = false;
+    this.remover.emit(this.tarefa);
+  }
+
+  onRemover() {
+    this.remover.emit(this.tarefa);
   }
 }
