7880b63735cfdcff496c36b6debfcd3eb27dd16d
Grasielly R <159390727+grasyzip@users.noreply.github.com>
2026-04-24 09:55:52 -0300

    Update tarefa.ts

--- src/app/tarefa.ts
+++ src/app/tarefa.ts
@@ -3,13 +3,13 @@ export class Tarefa {
     descricao: string;
     statusRealizada: boolean;
     completed: boolean;
-    importante: boolean;  
+    importante: boolean;
     
     constructor(descricao: string, statusRealizada: boolean, id?: string, completed: boolean = false, importante: boolean = false) {
         this.descricao = descricao;
         this.statusRealizada = statusRealizada;
         this._id = id;
         this.completed = completed;
-        this.importante = importante;  
+        this.importante = importante;
     }
 }
